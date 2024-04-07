1234814098e0beb8bc0b603bbbabf4c655c3daef
suhaibfida <Suhaibfida33@outlook.com>
2024-04-07 22:05:00 +0530

    Add Class for Electric bill system

---
+++ src/electricity/billing/system/Login.java
@@ -0,0 +1,111 @@
+package electricity.billing.system;
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JFrame{
+    
+    Login(){
+        //       ---------Creating Panel----------
+        setTitle("Flash Billing System");
+        setSize(750,590);
+        setLocation(410,100);
+   
+//       --------1st image background----------
+        ImageIcon background=new ImageIcon(ClassLoader.getSystemResource("icons/vivid-blurred-colorful-wallpaper-background.png") );
+        Image set1=background.getImage().getScaledInstance(750, 600, Image.SCALE_DEFAULT);
+        ImageIcon i1=new ImageIcon(set1);
+        JLabel image1=new JLabel(i1);
+        add(image1);
+//         -------------- 2nd Image Icon    -------------------
+        ImageIcon icon=new ImageIcon(ClassLoader.getSystemResource("icons/idea.png"));
+        Image set2=icon.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i2 =new ImageIcon(set2);
+         
+        JLabel image2=new JLabel(i2);
+        image2.setBounds(309, 20, 100, 100);
+        image2.setFont(new Font("SAN SERIF",Font.BOLD,30));
+        image1.add(image2);
+        
+//         --------Head Text--------
+         
+        JLabel headline=new JLabel("Flash Billing System");
+        headline.setBounds(215, 110, 800, 70);
+        headline.setFont(new Font("SAN SERIF",Font.BOLD,30));
+        headline.setForeground(Color.white);
+        image1.add(headline);
+        
+//        ----------- Logging As customer or admin -----------
+
+        Choice login = new Choice();
+        image1.add(login);
+        login.add(" Customer");
+        login.add(" Admin");
+        login.setBounds(10, 10, 100, 21);
+        login.setBackground(Color.orange);
+        login.setForeground(Color.DARK_GRAY);
+      
+//         --------------Input------------
+
+        JLabel text1=new JLabel("Username:");
+        text1.setBounds(245, 210, 300, 30);
+        text1.setForeground(Color.DARK_GRAY);
+        text1.setFont(new Font("SERIF",Font.BOLD,15));
+        image1.add(text1);
+        JTextField input1=new JTextField();
+        input1.setBounds(245, 240, 250, 21);
+        input1.setFont(new Font("SAN SERIF",Font.BOLD,12));
+        input1.setBorder(null);
+        input1.setBackground(Color.WHITE);
+        image1.add(input1);
+           
+        JLabel text2=new JLabel("Password:");
+        text2.setBounds(245, 285, 300, 30);
+        text2.setForeground(Color.DARK_GRAY);
+        text2.setFont(new Font("SERIF",Font.BOLD,15));
+        image1.add(text2);
+          
+        JTextField input2=new JTextField();
+        input2.setBounds(245, 315, 250, 21);
+        input2.setBorder(null);
+        input2.setBackground(Color.WHITE);
+        image1.add(input2);
+        
+//           ------------Buttons----------
+
+        JButton button1=new JButton("Sign In");
+        button1.setBounds(245, 385, 250, 28);
+        button1.setFont(new Font("SAN Serif",Font.BOLD,18));
+        button1.setForeground(Color.DARK_GRAY);
+        button1.setBackground(Color.ORANGE);
+        button1.setBorder(null);
+        image1.add(button1);
+        
+        JButton button2=new JButton("Sign Up");
+        button2.setForeground(Color.DARK_GRAY);
+        button2.setBackground(Color.LIGHT_GRAY);
+        button2.setBorder(null);
+        button2.setBounds(245, 420, 110, 25);
+        button2.setFont(new Font("SAN Serif",Font.BOLD,15));
+        image1.add(button2);
+        
+        JButton button3=new JButton("Forget");
+        button3.setForeground(Color.DARK_GRAY);
+        button3.setBackground(Color.LIGHT_GRAY);
+        button3.setBorder(null);
+        button3.setBounds(385, 420, 110, 25);
+        button3.setFont(new Font("SAN Serif",Font.BOLD,15));
+        image1.add(button3);
+           setVisible(true);
+//       -------Bottom------------
+
+        JLabel bottom=new JLabel("Created and designed by Suhaib Fida");
+        bottom.setBounds(267, 485, 500, 100);
+        bottom.setForeground(Color.white);
+        image1.add(bottom);
+           setLayout(null);
+        
+    }
+    public static void main(String args[]){
+        new Login(); 
+    }
+}
