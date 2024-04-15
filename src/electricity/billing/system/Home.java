f2d2ab5e4d34fb1fc2dff05679585c2e86ce1dd9
suhaibfida <Suhaibfida33@outlook.com>
2024-04-15 17:19:28 +0530

    Add menu bar

--- src/electricity/billing/system/Home.java
+++ src/electricity/billing/system/Home.java
@@ -2,6 +2,7 @@ package electricity.billing.system;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Home extends JFrame{
     Home(){
@@ -20,21 +21,30 @@ public class Home extends JFrame{
          JLabel image1=new JLabel(i1);
          add(image1);
          
-//         ---------------            ----------------
+//         --------------- Menu Bar ----------------
+          JMenuBar menu=new JMenuBar();
+          setJMenuBar(menu);
+          setLayout(new FlowLayout ());
+           
+          JMenu home=new JMenu("Home");
+          home.setFont(new Font("mono spaced",Font.BOLD,15));
+          menu.add(home);
+          
+          JMenuItem first=new JMenuItem("New Customer");
+          ImageIcon img2=new ImageIcon(ClassLoader.getSystemResource("icons/home.png"));
+          Image set2=img2.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+          first.setIcon(new ImageIcon(set2));
+          first.setMnemonic('A');
+          first.setFont(new Font("mono spaced",Font.PLAIN,12));
+          first.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,ActionEvent.CTRL_MASK));
+          home.add(first);
+          
+
          
          
          
          
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
+        
          
          
          setVisible(true);
